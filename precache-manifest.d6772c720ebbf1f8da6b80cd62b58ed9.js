599b34506e6af0d0cf5c88634bb4c0a2bd20ae55
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2021-07-05 19:45:56 +0530

    Updates

---
+++ precache-manifest.d6772c720ebbf1f8da6b80cd62b58ed9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd1ab6faf63a05226d91e324d88770c9",
+    "url": "/Front-End-Web-Development-with-React-Assignment-4/index.html"
+  },
+  {
+    "revision": "a7cea1d2b9a67c6b2923",
+    "url": "/Front-End-Web-Development-with-React-Assignment-4/static/css/2.ee30b40f.chunk.css"
+  },
+  {
+    "revision": "385b595e0fc952c769d8",
+    "url": "/Front-End-Web-Development-with-React-Assignment-4/static/css/main.23e744cf.chunk.css"
+  },
+  {
+    "revision": "a7cea1d2b9a67c6b2923",
+    "url": "/Front-End-Web-Development-with-React-Assignment-4/static/js/2.11000074.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/Front-End-Web-Development-with-React-Assignment-4/static/js/2.11000074.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "385b595e0fc952c769d8",
+    "url": "/Front-End-Web-Development-with-React-Assignment-4/static/js/main.c5473db0.chunk.js"
+  },
+  {
+    "revision": "8c35e2d1df92eb325da0",
+    "url": "/Front-End-Web-Development-with-React-Assignment-4/static/js/runtime-main.589e8af8.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Front-End-Web-Development-with-React-Assignment-4/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Front-End-Web-Development-with-React-Assignment-4/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Front-End-Web-Development-with-React-Assignment-4/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Front-End-Web-Development-with-React-Assignment-4/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Front-End-Web-Development-with-React-Assignment-4/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
